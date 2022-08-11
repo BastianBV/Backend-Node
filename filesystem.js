fce43e2c7bd6f73faefa64d8cc06652be15edaa2
BastianBV <fitzz.barker@gmail.com>
2022-08-10 19:10:46 -0500

    delete nodemodules

--- filesystem.js
+++ filesystem.js
@@ -1,10 +1,11 @@
 const fs = require("fs")
 console.log ("fs write", fs.write)
 
-fs.writeFile("text1.text", "Archivo creado desdenode", "utf8", (err) => {
+fs.writeFile("text1.txt", "Archivo creado desdenode", "utf8", (err) => {
     if(err) {
         console.log ("err", err)
         return //salte 
     }
     console.log("El archivo fue creado con exito")
 })
+
---
+++ fsReadSystem.js
@@ -0,0 +1,12 @@
+const fs = require ("fs")
+console.log("fs", fs.readFile )
+
+fs.readFile("text1.txt", "utf8", (err, data)=>{
+    // if(data) throw err; 
+    //     console.log("data", data);
+    if (err){
+        console.log("error", err)
+    }else {
+        console.log("data", data)
+    }
+})
